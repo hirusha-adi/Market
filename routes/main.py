840b6ef1941000acf9d58197c178c6191bb8f6db
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-28 23:47:50 +0530

    mobile detection

--- routes/main.py
+++ routes/main.py
@@ -1,5 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 
 
 def index():
-    return render_template('items/desktop.html')
+
+    user_agent = request.headers.get('User-Agent').lower()
+
+    if ("iphone" in user_agent) or ("android" in user_agent):
+        return render_template('items/mobile.html')
+    else:
+        return render_template('items/desktop.html')
