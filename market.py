ad9c8dd8c4fa6b1f8f778f6a72c559fee5f1cb4d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-29 13:16:20 +0530

    added new / route + support

--- market.py
+++ market.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.secret_key = "VerySecret12345"
 
 
-app.add_url_rule("/", 'index', index, methods=['GET'])
+app.add_url_rule("/", 'index_no_page', index_no_page, methods=['GET'])
+app.add_url_rule("/<page>", 'index', index, methods=['GET'])
 
 app.run("0.0.0.0", port=8080, debug=True)
