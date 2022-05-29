6e722b14d42ccbabfe2e02f6bcf442d171f30651
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-29 22:03:50 +0530

    Update test.py

--- utils/test.py
+++ utils/test.py
@@ -9,6 +9,9 @@ items = [
         "description": "9999 KM",
         "date": "2022-05-28",
         "thumnail": "https://cdn.discordapp.com/attachments/945257357867380747/980156250782306344/unknown.png",
+        "additional": {
+            ""
+        }
     },
     {
         "id": "12345",
