bf09b602457bd7bd48a9f9c7bb4088c5f31af0c9
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-02 17:40:26 +0530

    added url rule to register

--- market.py
+++ market.py
@@ -11,6 +11,7 @@ app.secret_key = "VerySecret12345"
 app.add_url_rule("/", 'index_no_page', index_no_page, methods=['GET'])
 app.add_url_rule("/<page>", 'index', index, methods=['GET'])
 app.add_url_rule("/login", 'login', login, methods=['GET', 'POST'])
+app.add_url_rule("/register", 'register', register, methods=['GET', 'POST'])
 app.add_url_rule("/profile", 'profile', profile, methods=['GET'])
 app.add_url_rule("/one_post", 'one_post', one_post, methods=['GET'])
 
