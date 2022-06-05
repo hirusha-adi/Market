469c68acd9aa5bb665b71be7df1d49a046c350b3
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-05 11:56:47 +0530

    get post by id | typical

--- database/mongo.py
+++ database/mongo.py
@@ -427,3 +427,16 @@ class Posts:
         ):
             temp.append(post)
         return temp
+
+    def getPostByID(id):
+        temp = []
+        for post in posts.find(
+            {
+                "id": int(id)
+            }
+        ):
+            temp.append(post)
+        try:
+            return temp[0]
+        except:
+            return False
