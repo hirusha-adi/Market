83f9bc308323128dfc5953ea0a7d50f03bc6b18d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-05 11:54:28 +0530

    get posts by name

--- database/mongo.py
+++ database/mongo.py
@@ -397,3 +397,17 @@ class Posts:
                 return temp[0]
             except:
                 return False
+
+        def getPostsByName(name):
+            temp = []
+            for post in posts.find(
+                {
+                    "name": {
+                        "$regex": f'.*{name}*.',
+                        "$options": 'i'  # ignore case
+                    },
+                    "type": "parts"
+                }
+            ):
+                temp.append(post)
+            return temp
