04080bf402b1c979b13d26e9af3c55c782e3a49b
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-11 13:50:42 +0530

    get proper image

--- routes/post.py
+++ routes/post.py
@@ -37,9 +37,7 @@ def one_post(id):
     except:
         return redirect(url_for('index_no_page'))
 
-    data['imgs'] = ["https://www.w3schools.com/w3css/img_mountains.jpg",
-                    "https://www.w3schools.com/w3css/img_snowtops.jpg",
-                    "https://www.w3schools.com/w3css/img_forest.jpg"]
+    data['imgs'] = data['post_info']['images']
 
     return render_template('post/mobile.html', **data)
 
